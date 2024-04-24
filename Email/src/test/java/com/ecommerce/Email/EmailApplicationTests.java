f443758412c95cb1ff3f84133c9af92981b1a317
Pratik-sys <pithore.pratik@gmail.com>
2024-04-24 16:27:08 +0530

    setup microservice

---
+++ Email/src/test/java/com/ecommerce/Email/EmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.Email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
